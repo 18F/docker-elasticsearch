b5f7b077640ed611af6bbfa2ff5696c8e5559dd0
Ozzy Johnson <oswald.johnson@gsa.gov>
2015-05-08 11:54:28 -0400

    fix first run script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,12 @@
 CONFIG_DIR=/elasticsearch/config
 
 # Set the required login credentials.
+echo "Running setup..."
 if [[ -e /.firstrun ]]; then
-  sed -i -e"s/^ES_USER/$ES_USERNAME/" $CONFIG_DIR/elasticsearch.yml
-  sed -i -e"s/^ES_PASS/$ES_PASSWORD/" $CONFIG_DIR/elasticsearch.yml
-  touch /.firstrun
+  echo "Looks like a first run..."
+  sed -i -e"s/ES_USER/$ES_USERNAME/" $CONFIG_DIR/elasticsearch.yml
+  sed -i -e"s/ES_PASS/$ES_PASSWORD/" $CONFIG_DIR/elasticsearch.yml
+  rm /.firstrun
 fi
 
-
 /elasticsearch/bin/elasticsearch
-
